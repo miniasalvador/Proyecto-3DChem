b05a4ff714147a7e726660782476433c2211bc0f
miniasalvador <55849224+miniasalvador@users.noreply.github.com>
2021-06-29 12:29:57 -0500

    Botones

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Events : MonoBehaviour
+{
+    public Transform scalarPanel;
+    public RectTransform contentTransform;
+    public float timeAnimation;
+
+    
+    public void OnMouseDown()
+    {
+        scalarPanel.DOScale(0.007f,timeAnimation);
+        contentTransform.localPosition = new Vector2 (0,0); 
+    
+    }
+
+    public void QuitarInfo() 
+    {   
+        scalarPanel.DOScale(0f,timeAnimation);
+    }
+}
+
+
+    

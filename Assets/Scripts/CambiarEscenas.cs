c14813b5f71a7c6fec8abf7ad6a8e5a9d6ff3920
miniasalvador <55849224+miniasalvador@users.noreply.github.com>
2021-07-28 01:08:15 -0500

    Avance Interfaces

---
+++ Assets/Scripts/CambiarEscenas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscenas : MonoBehaviour
+{
+
+    public void RegresarMenu() 
+    {
+        SceneManager.LoadScene("Inicio"); 
+    }
+    
+    public void AtomoScene()
+    {
+        SceneManager.LoadScene("Atomo");
+    }
+
+    public void TablaPScene() 
+    {
+        SceneManager.LoadScene("TablaP");
+    }
+    public void CompsScene() 
+    {
+        SceneManager.LoadScene("Comps");
+    }
+
+    public void CerrarApp()
+    {
+        Application.Quit();
+    }
+
+}
